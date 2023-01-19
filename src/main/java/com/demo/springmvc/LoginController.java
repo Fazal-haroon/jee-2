6c00e65109a649a6e891fde234968e680f1ed318
Fazal Haroon <kharoon187@gmail.com>
2023-01-20 01:18:19 +0400

    viewResolver view - spring MVC

--- src/main/java/com/demo/springmvc/LoginController.java
+++ src/main/java/com/demo/springmvc/LoginController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
     @RequestMapping(value = "/login")
-    @ResponseBody
+//    @ResponseBody
     public String sayHello() {
-        return "Hello World";
+        return "login";
     }
 }
