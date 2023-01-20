ba7b31314af51116270ec5d0bc3896e91a77507b
Fazal Haroon <kharoon187@gmail.com>
2023-01-20 23:47:23 +0400

    Redirect to Welcome page - ModelMap and @RequestParam in Spring MVC

--- src/main/java/com/demo/springmvc/LoginController.java
+++ src/main/java/com/demo/springmvc/LoginController.java
@@ -1,15 +1,25 @@
 package com.demo.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
 //    @ResponseBody
-    public String sayHello() {
+    public String showLoginPage() {
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String handleLoginRequest(@RequestParam(name = "name") String name, ModelMap modelMap){
+        System.out.println("name = " + name);
+        modelMap.put("name", name);
+        return "welcome";
     }
 }
