c6d7500e7852100e75ed6f1c29ca7d1f9822a478
Fazal Haroon <kharoon187@gmail.com>
2023-01-21 14:54:34 +0400

    code refactor

--- src/main/java/com/demo/springmvc/LoginController.java
+++ src/main/java/com/demo/springmvc/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
